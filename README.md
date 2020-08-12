740b1f35f428ba21c53e3fdb5a00259f443b1c7e
fansenze <151665030@qq.com>
2020-08-12 16:09:24 +0800

    🎉 1.0

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # snowpack-plugin-url-loader
 url loader for snowpack, use base64 encode file
+
+## Usage
+
+### Install
+```bash
+npm install --save-dev snowpack-plugin-url-loader
+```
+
+### Configuration
+add this plugin to your Snowpack config:  
+
+#### Commonly
+**snowpack.config.json**
+```json
+{
+  "plugins": [
+    ["snowpack-plugin-url-loader"]
+  ]
+}
+```
+
+#### Custom
+**snowpack.config.json**
+```json
+{
+  "plugins": [
+    ["snowpack-plugin-url-loader", {
+      "exts": ["jpg", "png"], // only "jpg" & "png" will be inlined with
+      "limit": 66666, // set maximum of file size: 66666 bytes
+    }]
+  ]
+}
+```
+
+## Options
+
+|             Name              |            Type             |                 Default                    | Description                                                                         |
+| :---------------------------: | :-------------------------: | :----------------------------------------: | :---------------------------------------------------------------------------------- |
+|     **[`exts`](#limit)**      |       `Array<string>`       |       `["jpg","jpeg","png","svg"]`         | Specifying the extensions of a file will be inlined with.                           |
+|     **[`limit`](#limit)**     | `boolean | number | string` |                 `10240`                    | Specifying the maximum size of a file in bytes.                                     |
+|  **[`encoding`](#encoding)**  |          `string`           |                 `base64`                   | Specify the encoding which the file will be inlined with.                           |
+
+
---
+++ plugin.js
@@ -0,0 +1,125 @@
+const mime = require('mime')
+const path = require('path');
+const { promises: fs } = require('fs');
+
+const pkg = require('./package.json');
+
+const defaultExts = ['jpg', 'jpeg', 'png', 'svg'];
+const defaultLimit = 10240;
+const defaultEncoding = 'base64';
+
+async function encode(file, name, { limit = defaultLimit, bufferEncoding = defaultEncoding }) {
+  const encoding = 'binary';
+  if (path.isAbsolute(file)) {
+    file = await fs.readFile(file, encoding);
+  }
+
+  if (file.length > limit) {
+    return;
+  }
+
+  const mimetype = mime.getType(name);
+  const buffer = Buffer.from(file, encoding);
+  return `data:${mimetype || ''};base64,${buffer.toString(bufferEncoding)}`;
+}
+
+/**
+ * @typedef {object} IPluginOptions
+ * @property {string[]} options.exts - default ["jpg","jpeg","png","svg"]
+ * @property {string|number} options.limit - default 10240
+ * @property {string} options.encoding - default "base64"
+ */
+
+/**
+ * @param {IPluginOptions} options 
+ * @returns {IPluginOptions}
+ */
+function formatOptions(options) {
+  const {
+    exts = defaultExts,
+    limit = defaultLimit,
+    encoding = defaultEncoding,
+  } = options || {};
+
+  return {
+    exts: Array.isArray(exts) && exts.length > 0 ? exts : defaultExts,
+    limit: limit === false ? 0 : Number(limit === true ? defaultLimit : limit),
+    encoding: encoding || defaultEncoding,
+  };
+}
+
+function getEncodingType(ext) {
+  const UTF8_FORMATS = ['.css', '.html', '.js', '.map', '.mjs', '.json', '.svg', '.txt', '.xml'];
+  return UTF8_FORMATS.includes(ext) ? 'utf-8' : 'binary';
+}
+
+/**
+ * @param {any} snowpackConfig 
+ * @param {IPluginOptions} options
+ */
+module.exports = function plugin(snowpackConfig, options) {
+  const {
+    exts,
+    limit,
+    encoding,
+  } = formatOptions(options);
+
+  const input = exts
+    .filter(ext => typeof ext === 'string' && !!ext)
+    .map(ext => ext.startsWith('.') ? ext : `.${ext}`);
+
+  const output = ['.js'].concat(input);
+
+  const cwd = process.cwd();
+
+  let isDev = true;
+
+  return {
+    name: pkg.name,
+    resolve: {
+      input,
+      output,
+    },
+    async run(options) {
+      isDev = !!options.isDev;
+    },
+    async load({ filePath, fileExt }) {
+      const uri = await encode(
+        filePath,
+        filePath,
+        { limit, bufferEncoding: encoding },
+      );
+      if (uri) {
+        console.log('[Url-Loader] Inlined File: ', filePath);
+        return {
+          '.js': `export default "${uri}";`,
+        };
+      } else {
+        const { sep } = path.posix;
+        const rootpath = filePath.replace(cwd + sep, '');
+        let webUri = rootpath;
+
+        // replace base proxy url
+        for (const [starts, dir] of Object.entries(snowpackConfig.mount)) {
+          if (rootpath.startsWith(starts)) {
+            webUri = rootpath.replace(starts, dir.endsWith(sep) ? dir : `${dir}${sep}`);
+            break;
+          }
+        }
+
+        if (!isDev) {
+          // copy file while build with snowpack
+          const { out } = snowpackConfig.devOptions;
+          const dest = path.posix.join(out, webUri);
+          fs.copyFile(filePath, dest);
+        }
+
+        return {
+          '.js': `export default "${webUri}";`,
+          [fileExt]: await fs.readFile(filePath, getEncodingType(fileExt)),
+        };
+      }
+    },
+  };
+}
+
